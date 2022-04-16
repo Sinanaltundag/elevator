6e94220f6826f43966d61f25039893f602bfbae5
peykani <peykani@gmail.com>
2022-04-17 01:28:34 +0300

    ok

--- src/box.jsx
+++ src/box.jsx
@@ -11,8 +11,8 @@ const ElevatorBox = ({item,arr,activeBox,setActiveBox, clickedNums,setClickedNum
   return (
     <div className="elevatorBox">
     <div>
-        <button onClick={()=>activeBox===item&&item<5&& setTimeout(()=>setActiveBox(activeBox+1),1000) }>up</button>
-        <button onClick={()=>activeBox===item&&item>0&&setTimeout(()=>setActiveBox(activeBox-1),1000)}>down</button>
+        <div className="up" onClick={()=>activeBox===item&&item<5&& setTimeout(()=>setActiveBox(activeBox+1),500) }></div>
+        <div className="down" onClick={()=>activeBox===item&&item>0&&setTimeout(()=>setActiveBox(activeBox-1),500)}></div>
     </div>
       <div className={`elevator ${activeBox===item&&"active"}`} >
           {activeBox===item&&arr.map(num=><button key={num} onClick={()=>handleClick(num)} className={activeBox===num?"activeSm":null}>{num}</button>)}
