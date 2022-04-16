758a823a1e8432103c7d9b3cf4371a29c6007334
peykani <peykani@gmail.com>
2022-04-17 01:00:20 +0300

    settimeout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import ElevatorBox from './box';
 
 function App() {
 const [activeBox, setActiveBox] = useState(0)
-const [clickedNums, setClickedNums] = useState([])
+const [clickedNums, setClickedNums] = useState([0])
+const aclick = useRef([])
   const arr=[0,1,2,3,4,5]
+  
+  // setClickedNums(activeBox>1?clickedNums.sort((a, b) => a - b):[0])
+  const output = clickedNums.reduce((prev, curr) => Math.abs(curr - activeBox) < Math.abs(prev - activeBox) ? curr : prev);
+  console.log(output)
+  
+  useEffect(() => {
+    setActiveBox(output)
+    if (clickedNums.length>1) {
+      setTimeout(() => {
+     setClickedNums(clickedNums.slice(1,clickedNums.length))
+   }, 2000);
+    }
+    
+    console.log(clickedNums)
+    }, [clickedNums])
 
   return (
     <div className="App">
     {arr.map(item =>
-      (<ElevatorBox key={item} item={item} arr={arr} activeBox={activeBox} setActiveBox={setActiveBox} clickedNums={clickedNums} setClickedNums={setClickedNums} />)
+      (<ElevatorBox key={item} item={item} arr={arr} activeBox={activeBox} setActiveBox={setActiveBox} clickedNums={clickedNums} setClickedNums={setClickedNums}  aclick={aclick}/>)
     )}
   
     </div>
--- src/box.jsx
+++ src/box.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+
 import "./App.css"
 
 const ElevatorBox = ({item,arr,activeBox,setActiveBox, clickedNums,setClickedNums}) => {
-    console.log(activeBox)
-const handleClick = () =>{
-    setClickedNums(clickedNums.push())
+    const handleClick = (num) =>{
+        setClickedNums([...clickedNums,num])
+       
 }
+
+
   return (
     <div className="elevatorBox">
     <div>
-        <button onClick={()=>activeBox===item&& setTimeout(()=>setActiveBox(activeBox+1),2000) }>up</button>
-        <button onClick={()=>activeBox===item&&setActiveBox(activeBox-1)}>down</button>
+        <button onClick={()=>activeBox===item&&item<5&& setTimeout(()=>setActiveBox(activeBox+1),1000) }>up</button>
+        <button onClick={()=>activeBox===item&&item>0&&setTimeout(()=>setActiveBox(activeBox-1),1000)}>down</button>
     </div>
       <div className={`elevator ${activeBox===item&&"active"}`} >
-          {activeBox===item&&arr.map(num=><button key={num} onClick={()=>setActiveBox(num)} className={activeBox===num&&"activeSm"}>{num}</button>)}
+          {activeBox===item&&arr.map(num=><button key={num} onClick={()=>handleClick(num)} className={activeBox===num?"activeSm":null}>{num}</button>)}
       </div>
     </div>
   );
